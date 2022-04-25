b0123640bcf75be38616210b9e9d3b666d0c272d
Juan Cabra <jscabra123@gmail.com>
2022-04-25 04:57:30 -0500

    menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { MarketingComponent } from './marketing/marketing.component';
+import { InfluencersComponent } from './influencers/influencers.component';
+import { AdministradoresComponent } from './administradores/administradores.component';
+import { CampaniasComponent } from './campanias/campanias.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    HomeComponent,
+    MarketingComponent,
+    InfluencersComponent,
+    AdministradoresComponent,
+    CampaniasComponent
   ],
   imports: [
     BrowserModule,
